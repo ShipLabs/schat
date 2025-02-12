68e946a0b80e6476b79f3b86594628c509349291
3cognito <tommyubongabasijohn@gmail.com>
2025-02-12 02:39:01 +0100

    setup app base

--- server/internal/base/init.go
+++ server/internal/base/init.go
@@ -5,3 +5,18 @@ import "gorm.io/gorm"
 type base struct {
 	db *gorm.DB
 }
+
+type baseHandlers struct {
+}
+
+func New(db *gorm.DB) *base {
+	return &base{
+		db: db,
+	}
+}
+
+func (b *base) MountHandlers() baseHandlers {
+	var h baseHandlers
+
+	return h
+}
