f3d9b1b89b57b765b694dc98ce1819a439b1fb91
3cognito <tommyubongabasijohn@gmail.com>
2025-02-13 04:18:52 +0100

    injection of messages repo

--- server/internal/base/repos.go
+++ server/internal/base/repos.go
@@ -5,3 +5,11 @@ import repos "shiplabs/schat/internal/repositories"
 func (b *base) WithUserRepo() repos.UserRepoInterface {
 	return repos.NewUserRepo(*b.db)
 }
+
+func (b *base) WithGroupMsgRepo() repos.GroupMessageRepoInterface {
+	return repos.NewGroupMessageRepo(*b.db)
+}
+
+func (b *base) WithPrivateMsgRepo() repos.PrivateMessageRepoInterface {
+	return repos.NewPrivateMessageRepo(*b.db)
+}
