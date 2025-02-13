9853883216e69a4ee7659d3f10c27a6d6fbeff60
3cognito <tommyubongabasijohn@gmail.com>
2025-02-13 16:43:55 +0100

    add private chat id

--- server/internal/models/private_chat.go
+++ server/internal/models/private_chat.go
@@ -7,6 +7,7 @@ import (
 
 type PrivateChat struct {
 	gorm.Model     `json:"-"`
+	ID             uuid.UUID `gorm:"primaryKey;type:uuid;default:uuid_generate_v4()" json:"id"`
 	FirstMemberID  uuid.UUID `gorm:"not null;index" json:"first_member_id"`
 	FirstMember    User      `gorm:"foreignKey:first_member_id" json:"-"`
 	SecondMemberID uuid.UUID `gorm:"not null;index" json:"second_member_id"`
