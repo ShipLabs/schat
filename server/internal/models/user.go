8ae7e405bead3568a626b486ae52fca933cb8dc8
3cognito <tommyubongabasijohn@gmail.com>
2025-02-13 03:30:19 +0100

    add username and modify id field

--- server/internal/models/user.go
+++ server/internal/models/user.go
@@ -7,7 +7,8 @@ import (
 
 type User struct {
 	gorm.Model `json:"-"`
-	ID         uuid.UUID `gorm:"primaryKey" json:"id"`
+	ID         uuid.UUID `gorm:"primaryKey;type:uuid;default:uuid_generate_v4()" json:"id"`
+	UserName   string    `gorm:"not null;uniqueIndex" json:"username"`
 	Name       string    `gorm:"not null" json:"name"`
 	Email      string    `gorm:"not null;uniqueIndex" json:"email"`
 	Password   string    `gorm:"not null" json:"password"`
