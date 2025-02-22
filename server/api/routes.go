de9d9d60a2741e30390589463fdd339f55a90cf3
3cognito <tommyubongabasijohn@gmail.com>
2025-02-22 17:46:53 +0100

    uncomment auth middleware

--- server/api/routes.go
+++ server/api/routes.go
@@ -2,6 +2,7 @@ package api
 
 import (
 	"shiplabs/schat/internal/base"
+	"shiplabs/schat/internal/middlewares"
 	"shiplabs/schat/internal/pkg/db"
 	"shiplabs/schat/internal/pkg/store"
 
@@ -12,7 +13,7 @@ func RoutesHandler(e *gin.Engine) {
 	app := base.New(db.DB, store.WebsocketStore).MountHandlers()
 
 	v1 := e.Group("api/v1")
-	authRequired := v1.Group("").Use( /*middlewares.Auth*/ )
+	authRequired := v1.Group("").Use(middlewares.Auth)
 
 	v1.POST("/register", app.AuthH.SignUp)
 	v1.POST("/login", app.AuthH.Login)
