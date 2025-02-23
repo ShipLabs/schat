72462728fa01b1b04b6d24bb7987e04b76893e99
3cognito <tommyubongabasijohn@gmail.com>
2025-02-23 17:02:35 +0100

    handle, chat and group service and member management

--- server/internal/handlers/ws.go
+++ server/internal/handlers/ws.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"shiplabs/schat/internal/pkg/store"
 	"shiplabs/schat/internal/services"
+	"shiplabs/schat/pkg/shared"
 	"sync"
 
 	"github.com/gin-gonic/gin"
@@ -15,6 +16,7 @@ import (
 
 var (
 	ErrHandShakeFail = errors.New("failed handshake, connection not established")
+	ErrInvalidGroup  = errors.New("invalid group")
 )
 
 type WSResponse struct {
@@ -24,17 +26,17 @@ type WSResponse struct {
 }
 
 type wsHandler struct {
-	store              store.ConnectionStoreInterface
-	privateChatService services.ChatServiceInterface
-	groupService       services.GroupServiceInterface
+	store        store.ConnectionStoreInterface
+	chatService  services.ChatServiceInterface
+	groupService services.GroupServiceInterface
 }
 
 type WsHandlerInterface interface {
 	Connect(userID uuid.UUID, ctx *gin.Context) (*websocket.Conn, error)
 	HandlePrivateChat(ctx *gin.Context)
 	GroupCreationHandler(ctx *gin.Context)
-	// HandleGroupChat(ctx *gin.Context)
-	// HandlerGroupCreation(ctx *gin.Context)
+	HandleGroupChat(ctx *gin.Context)
+	HandleMembership(ctx *gin.Context)
 }
 
 func NewWebSocketHandler(
@@ -43,9 +45,9 @@ func NewWebSocketHandler(
 	groupService services.GroupServiceInterface,
 ) WsHandlerInterface {
 	return &wsHandler{
-		store:              store,
-		privateChatService: pChatService,
-		groupService:       groupService,
+		store:        store,
+		chatService:  pChatService,
+		groupService: groupService,
 	}
 }
 
@@ -76,9 +78,10 @@ func (w *wsHandler) HandlePrivateChat(ctx *gin.Context) {
 	userID := uuid.MustParse(ctx.GetString("userID"))
 	conn, err := w.Connect(userID, ctx)
 	if err != nil {
-		return //how do I better handle error here?? send back http json seems best since ws conn has not been established
+		shared.ErrorResponse(ctx, http.StatusUnprocessableEntity, err.Error())
+		return
 	}
-	defer w.closeConn(conn, userID) //should I close the connection here???
+	defer w.closeConn(conn, userID)
 
 	for {
 		var message services.PrivateMessageDto
@@ -93,9 +96,23 @@ func (w *wsHandler) HandlePrivateChat(ctx *gin.Context) {
 }
 
 func (w *wsHandler) HandleGroupChat(ctx *gin.Context) {
-	_, err := upgrader.Upgrade(ctx.Writer, ctx.Request, nil)
+	userID := uuid.MustParse(ctx.GetString("userID"))
+	conn, err := w.Connect(userID, ctx)
 	if err != nil {
-		//handle
+		shared.ErrorResponse(ctx, http.StatusUnprocessableEntity, err.Error())
+		return
+	}
+	defer w.closeConn(conn, userID)
+
+	for {
+		var message services.GroupMessageDto
+		if err := conn.ReadJSON(&message); err != nil {
+			//TOD: check unepected connection closure error
+			log.Println(err)
+			w.handleResponse(conn, http.StatusBadRequest, err.Error(), "")
+		}
+
+		go w.handleGroupMessage(&message, conn)
 	}
 }
 
@@ -103,7 +120,8 @@ func (w *wsHandler) GroupCreationHandler(ctx *gin.Context) {
 	userID := uuid.MustParse(ctx.GetString("userID"))
 	conn, err := w.Connect(userID, ctx)
 	if err != nil {
-		return //how do I better handle error here?? send back http json seems best since ws conn has not been established
+		shared.ErrorResponse(ctx, http.StatusUnprocessableEntity, err.Error())
+		return
 	}
 	defer w.closeConn(conn, userID)
 
@@ -119,6 +137,60 @@ func (w *wsHandler) GroupCreationHandler(ctx *gin.Context) {
 	}
 }
 
+func (w *wsHandler) HandleMembership(ctx *gin.Context) {
+	userID := uuid.MustParse(ctx.GetString("userID"))
+	groupID, err := uuid.Parse(ctx.Param("group_id"))
+	if err != nil {
+		shared.ErrorResponse(ctx, http.StatusUnprocessableEntity, "invalid group id")
+		return
+	}
+	conn, err := w.Connect(userID, ctx)
+	if err != nil {
+		return
+	}
+	defer w.closeConn(conn, userID)
+
+	for {
+		var message services.GroupMembershipDto
+		if err := conn.ReadJSON(&message); err != nil {
+			//TOD: check unepected connection closure error
+			log.Println(err)
+			w.handleResponse(conn, http.StatusBadRequest, err.Error(), "")
+		}
+
+		go w.handleGroupMemberShip(userID, groupID, &message, conn)
+	}
+}
+func (w *wsHandler) handleGroupMemberShip(userID, groupID uuid.UUID, data *services.GroupMembershipDto, createrConn *websocket.Conn) {
+	memberID, err := uuid.Parse(data.MemberID)
+	if err != nil {
+		log.Println(err)
+		w.handleResponse(createrConn, http.StatusBadRequest, "invalid member id", "")
+		return
+	}
+	if err := w.groupService.HandleMembership(groupID, userID, memberID, data.Action); err != nil {
+		log.Println(err)
+		w.handleResponse(createrConn, http.StatusBadRequest, "invalid member id", "")
+		return
+	}
+
+	members, err := w.groupService.GetGroupMembers(groupID)
+	if err != nil {
+		log.Println(err)
+		//group has been created but members could not be fetched for notification, how to best handle???
+		w.handleResponse(createrConn, http.StatusBadRequest, err.Error(), "")
+		return
+	}
+
+	var wg sync.WaitGroup
+	for _, member := range members {
+		wg.Add(1)
+		go w.groupMembershipNotification(member.UserID, data, &wg)
+	}
+
+	wg.Wait() //any need for the wg????
+}
+
 func (w *wsHandler) handleGroupCreation(userID uuid.UUID, data *services.CreateGroupDto, createrConn *websocket.Conn) {
 	if err := w.groupService.CreateGroup(userID, *data); err != nil {
 		log.Println(err)
@@ -127,14 +199,80 @@ func (w *wsHandler) handleGroupCreation(userID uuid.UUID, data *services.CreateG
 
 	var wg sync.WaitGroup
 	for _, memberID := range data.Members {
+		//handle this better - find a way
+		memberUUID, err := uuid.Parse(memberID)
+		if err != nil {
+			log.Println("invalid member UUID:", memberID)
+			continue
+		}
 		wg.Add(1)
-		go w.notifyOnlineUser(memberID, data, &wg)
+		go w.groupCreationNotification(memberUUID, data, &wg)
 	}
 
-	wg.Wait()
+	wg.Wait() //any need for the wg????
 }
 
-func (w *wsHandler) notifyOnlineUser(userID uuid.UUID, data *services.CreateGroupDto, wg *sync.WaitGroup) {
+func (w *wsHandler) handleGroupMessage(data *services.GroupMessageDto, senderConn *websocket.Conn) {
+	groupUUID, err := uuid.Parse(data.GroupID)
+	if err != nil {
+		log.Println(err)
+		w.handleResponse(senderConn, http.StatusBadRequest, err.Error(), "")
+		return
+	}
+	if err := w.chatService.SendMsgToGroup(*data); err != nil {
+		log.Println(err)
+		w.handleResponse(senderConn, http.StatusBadRequest, err.Error(), "")
+		return
+	}
+	members, err := w.groupService.GetGroupMembers(groupUUID)
+	if err != nil {
+		log.Println(err)
+		w.handleResponse(senderConn, http.StatusBadRequest, err.Error(), "")
+		return
+	}
+
+	var wg sync.WaitGroup
+	for _, member := range members {
+		wg.Add(1)
+		go w.groupMessageNotification(member.UserID, data, &wg)
+	}
+
+	wg.Wait() //any need for the wg????
+
+}
+
+func (w *wsHandler) groupMessageNotification(userID uuid.UUID, data *services.GroupMessageDto, wg *sync.WaitGroup) {
+	conn, err := w.store.GetConn(userID)
+	if err != nil {
+		log.Println("not online")
+		wg.Done()
+		return
+	}
+
+	w.handleResponse(conn, http.StatusOK, "", data.Content)
+	wg.Done()
+}
+
+func (w *wsHandler) groupMembershipNotification(userID uuid.UUID, data *services.GroupMembershipDto, wg *sync.WaitGroup) {
+	conn, err := w.store.GetConn(userID)
+	if err != nil {
+		log.Println("not online")
+		wg.Done()
+		return
+	}
+
+	var action string
+	if data.Action == services.Add {
+		action += "ed"
+	} else {
+		action += "d"
+	}
+
+	w.handleResponse(conn, http.StatusOK, "", data.MemberID+" has been "+action)
+	wg.Done()
+}
+
+func (w *wsHandler) groupCreationNotification(userID uuid.UUID, data *services.CreateGroupDto, wg *sync.WaitGroup) {
 	conn, err := w.store.GetConn(userID)
 	if err != nil {
 		log.Println("not online")
@@ -148,7 +286,7 @@ func (w *wsHandler) notifyOnlineUser(userID uuid.UUID, data *services.CreateGrou
 }
 
 func (w *wsHandler) handlerIncomingPrivateMsg(message *services.PrivateMessageDto, senderConn *websocket.Conn) {
-	if err := w.privateChatService.SendPrivateMsg(*message); err != nil {
+	if err := w.chatService.SendPrivateMsg(*message); err != nil {
 		log.Println(err)
 		w.handleResponse(senderConn, http.StatusBadRequest, err.Error(), "")
 	}
